57d9394f5cb735bf252ba39261da7583f8afec33
Julien Delvaux <delvaux.ju@gmail.com>
2016-03-31 10:47:17 +0200

    Add registers for offset calibration Comments to describe the register Fixed CTRL_REG defines

--- PIC/sensors/FXLS8471Q_registers.h
+++ PIC/sensors/FXLS8471Q_registers.h
@@ -25,9 +25,19 @@ LICENSE:
 #ifndef FXLS8471Q_REGISTERS_H
 #define	FXLS8471Q_REGISTERS_H
 
+// Device ID & Device Address
 #define FXLS8471Q_ADDRESS       0x1E
 #define FXLS8471Q_DEVICE_ID     0x6A
 
+// Output Data Register
+#define FXLS8471Q_OUT_X_MSB 0x01
+#define FXLS8471Q_OUT_X_LSB 0x02
+#define FXLS8471Q_OUT_Y_MSB 0x03
+#define FXLS8471Q_OUT_Y_LSB 0x04
+#define FXLS8471Q_OUT_Z_MSB 0x05
+#define FXLS8471Q_OUT_Z_LSB 0x06
+
+// Who Am I
 #define FXLS8471Q_WHO_AM_I      0x0D  
 #define FXLS8471Q_WHO_AM_I_BIT        7
 #define FXLS8471Q_WHO_AM_I_LENGTH     8
@@ -36,9 +46,9 @@ LICENSE:
 #define FXLS8471Q_CTRL_REG1     0x2A   
 #define FXLS8471Q_CTRL_REG1_active_BIT     0 
 #define FXLS8471Q_CTRL_REG1_active_LENGTH  1
-#define FXLS8471Q_CTRL_REG1_ASPL_BIT     6 
+#define FXLS8471Q_CTRL_REG1_ASPL_BIT     7 
 #define FXLS8471Q_CTRL_REG1_ASPL_LENGTH  2 
-#define FXLS8471Q_CTRL_REG1_ODR_BIT      3 
+#define FXLS8471Q_CTRL_REG1_ODR_BIT      5 
 #define FXLS8471Q_CTRL_REG1_ODR_LENGTH   3 
 
 // CTRL_REG2 configuration
@@ -55,6 +65,10 @@ LICENSE:
 #define FXLS8471Q_XYZ_fs_BIT    0x0
 #define FXLS8471Q_XYZ_fs_LENGTH 2
 
+// Offset correction
+#define FXLS8471Q_OFF_X 0x2F
+#define FXLS8471Q_OFF_Y 0x30
+#define FXLS8471Q_OFF_Z 0x31
 
 #define FXLS8471Q_CTRL_REG3     0x2C
 #define FXLS8471Q_CTRL_REG4     0x2D
